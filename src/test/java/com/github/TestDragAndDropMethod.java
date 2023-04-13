6035c9f3a112337042e293ba8350f8f8a1cca5ca
d.ochkasov <d.ochkasov@qiwi.com>
2023-04-13 18:33:21 +0300

    added DragAndDrop

---
+++ src/test/java/com/github/TestDragAndDropMethod.java
@@ -0,0 +1,17 @@
+package com.github;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestDragAndDropMethod extends TestBase {
+    @org.junit.jupiter.api.Test
+    void DragAndDropMethod() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+   }
+}
